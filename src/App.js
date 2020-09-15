b4508ba46be3378b332c90e4a0b40e16ba0e28e9
Ryo Igarashi <n33t5hin@gmail.com>
2020-09-15 15:59:29 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Typography, Grid, Button, Paper, Input, Slider } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [dir, setDir] = useState("column");
+
+  const toggle = () => {
+    if (dir === "column") {
+      return setDir("row");
+    }
+
+    return setDir("column");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography variant="h2">Hello Material UI</Typography>
+
+      <Button onClick={toggle} color="primary" variant="contained">
+        Toggle Appearance
+      </Button>
+
+      <Grid direction={dir} container spacing={3} justify="center" alignItems="center">
+        <Grid item>
+          <Paper variant="elevation">foo</Paper>
+        </Grid>
+
+        <Grid item>
+          <Paper variant="elevation">bar</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
